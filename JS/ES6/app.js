ee00267455a3e21e4c37639dc65044c1c203a753
juancca90 <castanedacamilo819@gmail.com>
2024-01-21 18:50:05 -0500

    Iteration#2.4

--- JS/ES6/app.js
+++ JS/ES6/app.js
@@ -50,3 +50,33 @@ const [fruit1, fruit2, fruit3] = fruits;
 console.log(fruit1);
 console.log(fruit2);
 console.log(fruit3);
+
+//!---------------------------------
+//? 2.3 En base al siguiente javascript, usa destructuring para crear 2
+//? variables igualandolo a la función e imprimiendolo por consola.
+
+const animalFunction = () => {
+return {nameAn: 'Bengal Tiger', race: 'Tiger'}
+};
+const {nameAn, race} = animalFunction();
+console.log(nameAn);
+console.log(race);
+
+
+//!----------------------------------------
+//? 2.4 En base al siguiente javascript, usa destructuring para crear las
+//? variables name y itv con sus respectivos valores. Posteriormente crea
+//? 3 variables usando igualmente el destructuring para cada uno de los años
+//? y comprueba que todo esta bien imprimiendolo.
+
+const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] }
+
+const {name, itv} = car;
+
+const [year1, year2, year3]= itv;
+
+console.log(name);
+console.log(itv);
+console.log(year1);
+console.log(year2);
+console.log(year3);
